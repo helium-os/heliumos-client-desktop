11b4d8d4abcbd2ed01ebf986481eb8e04f291b0b
songluming <konglq@easypayx.com>
2023-04-19 15:49:54 +0800

    first push

---
+++ main.js
@@ -0,0 +1,253 @@
+const { ipcMain, app, BrowserWindow, dialog, globalShortcut } = require("electron");
+const path = require("path");
+var os = require("os");
+const url = require("url");
+const fetch = require("electron-fetch").default;
+let a = true;
+const storage = require("electron-json-storage");
+let { autoUpdater } = require("electron-updater");
+var crypto  = require('crypto')
+var fs      = require('fs')
+const forge = require('node-forge');
+var keyList=["heliumos.crt",'../heliumos.crt']
+var publicKey
+
+// app.commandLine.appendSwitch('disk-cache-size',20000000)
+app.commandLine.appendSwitch('no-proxy-server')
+// app.commandLine.appendSwitch('disable-http2')
+
+keyList.forEach(item=>{
+  if( fs.existsSync(path.join(__dirname, item))){
+    publicKey=fs.readFileSync(path.join(__dirname, item), 'utf8')
+  }
+})
+ let datas = {}
+createWindow = async () => {
+  //  清除store
+  //   storage.clear(function(error) {
+  //   if (error) throw error;
+  // })
+
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    minWidth: 800,
+    minHeight: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false, //禁用同源策略
+
+      plugins: true, //是否支持插件
+
+      nativeWindowOpen: true, //是否使用原生的window.open()
+
+      webviewTag: true, //是否启用 <webview> tag标签
+
+      sandbox: true, //沙盒选项,这个很重要
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  autoUpdater.autoDownload = true;
+  autoUpdater.setFeedURL({
+    provider: "github",
+    owner: "helium-os",
+    repo: "electron",
+    releaseType: "release",
+    private: true,
+    token: "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",
+  });
+  autoUpdater.checkForUpdatesAndNotify();
+
+  // autoUpdater.signals.updateDownloaded(() => {})
+  //   autoUpdater.on('error', (error) => {
+  //     console.log('检查更新失败: ' + error == null ? 'unknown' : (error.stack || error).toString())
+
+  //   })
+  //  autoUpdater.checkForUpdates()
+
+  //   autoUpdater.on('update-available', (info) => {
+
+  //     // dialog.showMessageBox({
+  //     //   type: 'info',
+  //     //   title: '更新提示',
+  //     //   message: '软件需要更新，您是否立即更新？',
+  //     //   buttons: ['推迟', '立即更新']
+  //     // }).then((res) => {
+  //     //   console.log('index:' + res.response)
+  //     //   if (res.response === 1) {
+  //     //     console.log('选择升级')
+  //     //     autoUpdater.downloadUpdate()
+  //     //   } else {
+  //     //     console.log('选择不升级:')
+  //     //   }
+  //     // })
+  //   })
+
+  //   // 检查更新时触发
+  //   autoUpdater.on('update-available', (res) => {
+  //     console.log('检查更新时触发')
+
+  //   })
+
+  //   // 没有可用更新
+  //   autoUpdater.on('update-not-available', () => {
+  //     console.log('没有可用更新')
+
+  //   })
+
+  //   // 安装更新
+  //   autoUpdater.on('update-downloaded', (res) => {
+
+  //     autoUpdater.quitAndInstall()
+  //   })
+
+  ipcMain.on("ping", function (event, arg) {
+    event.returnValue = "pong";
+  });
+
+  ipcMain.on("iframeUP", function (event) {
+    win.setAlwaysOnTop(true);
+  });
+
+  ipcMain.on("iframeDown", function (event) {
+    win.setAlwaysOnTop(false);
+  });
+
+  ipcMain.on("setuserInfo", function (event, arg) {
+    let secureDnsServers = [];
+
+    if (arg.DNS) {
+      // secureDnsServers[0] = "https://dns." + arg?.DNS + ".helium";
+      //   app.configureHostResolver({
+      //  enableBuiltInResolver:false,
+      //  secureDnsMode: 'secure',
+      //  secureDnsServers
+      // })
+    }
+    
+    storage.get("data", function (error, data) {
+      datas={ ...data, ...arg }
+      storage.set("data", { ...data, ...arg });
+    });
+  });
+
+  ipcMain.on("setDNS", function (event, arg) {
+    storage.set("DNS", arg);
+  });
+  ipcMain.on('clearInfo',()=> win.loadFile("./index.html"))
+  //获取本地唯一MAC地址
+  var mac = "";
+  var networkInterfaces = os.networkInterfaces();
+  for (var i in networkInterfaces) {
+    for (var j in networkInterfaces[i]) {
+      if (
+        networkInterfaces[i][j]["family"] === "IPv4" &&
+        networkInterfaces[i][j]["mac"] !== "00:00:00:00:00:00" &&
+        networkInterfaces[i][j]["address"] !== "127.0.0.1"
+      ) {
+        mac = networkInterfaces[i][j]["mac"];
+      }
+    }
+  }
+    win.on('focus', () => {
+    // mac下快捷键失效的问题
+    if (process.platform === 'darwin') {
+      let contents = win.webContents
+      globalShortcut.register('CommandOrControl+C', () => {
+        console.log('注册复制快捷键成功')
+        contents.copy()
+      })
+
+      globalShortcut.register('CommandOrControl+V', () => {
+        console.log('注册粘贴快捷键成功')
+        contents.paste()
+      })
+
+      globalShortcut.register('CommandOrControl+X', () => {
+        console.log('注册剪切快捷键成功')
+        contents.cut()
+      })
+
+      globalShortcut.register('CommandOrControl+A', () => {
+        console.log('注册全选快捷键成功')
+        contents.selectAll()
+      })
+    }
+  })
+  win.on('blur', () => {
+    globalShortcut.unregisterAll() // 注销键盘事件
+  })
+  // win.loadURL('https://desktop.org1.helium');
+  // win.loadURL('http://192.168.50.120:5173');
+  win.loadFile("./index.html");
+  win.maximize(); 
+  // win.webContents.openDevTools();
+  //监听单页页面跳转（antd-pro这种）
+  //   win.webContents.on('did-navigate-in-page', (event,url) => {
+  //   console.log(url)
+  // });
+  //监听页面跳转
+  // win.webContents.on('will-navigate', (event,url) => {
+  //   console.log(event)
+  //   console.log(url)
+  // });
+};
+
+app.on(
+  "certificate-error",
+  (event, webContents, url, error, cert, callback) => {
+  let a=new  crypto.X509Certificate(publicKey);
+   let b=new  crypto.X509Certificate(cert.data);
+   if (a.issuer==b.issuer) {
+    event.preventDefault()
+    callback(true)
+  } else {
+    callback(false)
+  }
+}
+);
+
+app.whenReady().then(async () => {
+  let  secureDnsServers = ["https://dns-testinner.easypayx.com/dns-query"];
+  console.log(publicKey)
+  await storage.get("data", function (error, data) {
+    datas = data;
+    // if (datas?.DNS ) {
+    //   secureDnsServers[0] = 'https://dns.' + datas?.DNS + '.helium';
+    //     app.configureHostResolver({
+    //    enableBuiltInResolver:false,
+    //    secureDnsMode: 'secure',
+    //    secureDnsServers
+    //   })
+    // }
+  });
+   app.configureHostResolver({
+       enableBuiltInResolver:false,
+       secureDnsMode: 'secure',
+       secureDnsServers
+      })
+  if (!app.requestSingleInstanceLock()) {
+    app.quit();
+    return;
+  }
+
+  //dns配置
+
+  ipcMain.handle("getValue", async function (event, arg) {
+    return datas[arg] || "";
+  });
+
+  createWindow();
+  require("./menu.js");
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
