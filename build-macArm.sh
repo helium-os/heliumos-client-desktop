a3cd0d1351cf89c75ac4edde54c8e3f3096b8e7c
songluming <konglq@easypayx.com>
2023-04-19 17:36:58 +0800

    add push branch

--- build-macArm.sh
+++ build-macArm.sh
@@ -3,8 +3,5 @@ npm run version
 version=`cat version`
 sed -e "s/VERSION/${version}/" version.template.js > version.js
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-npm install npm@8.15.0 -g
-npm config set electron_mirror "https://npm.taobao.org/mirrors/electron/"
-npm install --registry=https://registry.npm.taobao.org/
 npm run dist-macArm
 sed -e "s/VERSION/${version}/" version.template.json > dist/version.json
