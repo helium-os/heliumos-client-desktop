738d379bdba9839d3e186d836ac3e4b3a76224d9
songluming <konglq@easypayx.com>
2023-08-28 11:16:23 +0800

    heliumos#309 changeCode

--- build-macArm.sh
+++ build-macArm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm run version
 version=`cat version`
-# sed -e "s/VERSION/${version}/" version.template.js > version.js
+sed -e "s/VERSION/${version}/" version.template.js > version.js
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 npm run dist-macArm
-# sed -e "s/VERSION/${version}/" version.template.json > dist/version.json
+sed -e "s/VERSION/${version}/" version.template.json > dist/version.json
