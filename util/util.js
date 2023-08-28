cedf500a33e1be01f9220b601b9876067ab1d6f6
songluming <konglq@easypayx.com>
2023-08-28 18:06:24 +0800

    heliumos#309 changeCode

--- util/util.js
+++ util/util.js
@@ -101,22 +101,22 @@ macShortcutKeyFailure = (win, globalShortcut) => {
     let contents = win.webContents
     globalShortcut.register('CommandOrControl+C', () => {
       console.log('注册复制快捷键成功')
-      contents?.copy()
+      contents&&contents?.copy()
     })
 
     globalShortcut.register('CommandOrControl+V', () => {
       console.log('注册粘贴快捷键成功')
-      contents?.paste()
+      contents&&contents?.paste()
     })
 
     globalShortcut.register('CommandOrControl+X', () => {
       console.log('注册剪切快捷键成功')
-      contents?.cut()
+      contents&&contents?.cut()
     })
 
     globalShortcut.register('CommandOrControl+A', () => {
       console.log('注册全选快捷键成功')
-      contents?.selectAll()
+      contents&&contents?.selectAll()
     })
   }
 }
