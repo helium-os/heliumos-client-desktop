2997c383162181c289e580dffa15dd66cf1d4aec
songluming <konglq@easypayx.com>
2023-12-21 11:54:53 +0800

    heliumos#666 changMacMenuIcon

--- app-init/setTray.js
+++ app-init/setTray.js
@@ -1,4 +1,4 @@
-const { app, Tray, Menu, nativeImage } = require("electron");
+const { app, Tray, Menu, nativeImage, systemPreferences } = require("electron");
 const util = require('../util/util');
 const path = require("path");
 //创建系统托盘 
@@ -20,6 +20,29 @@ let setTray = (win) => {
 
   // 双击托盘图标时显示应用
   tray.on('double-click', () => win.show());
+  if (process.platform === 'darwin') {
+    let lightModeTrayIcon = util.findPath(["./../lightModeTrayIcon.png", '../build/lightModeTrayIcon.png', '../../lightModeTrayIcon.png'], __dirname)
+    let darkModeTrayIcon = util.findPath(["./../darkModeTrayIcon.png", '../build/darkModeTrayIcon.png', '../../darkModeTrayIcon.png'], __dirname)
+    function updateTrayIcon() {
+      // 检测操作系统的外观设置
+      const isDarkMode = systemPreferences.isDarkMode();
+      // 根据外观设置选择相应的托盘图标
+      const trayIconPath = isDarkMode
+        ? path.join(__dirname, darkModeTrayIcon)
+        : path.join(__dirname, lightModeTrayIcon);
+
+      // 设置托盘图标
+      tray.setImage(trayIconPath);
+    }
+    // 检测外观设置变化
+    systemPreferences.subscribeNotification('AppleInterfaceThemeChangedNotification', () => {
+      updateTrayIcon();
+    });
+
+    // 初始化时更新托盘图标
+    updateTrayIcon();
+
+  }
 }
 
 module.exports = setTray
