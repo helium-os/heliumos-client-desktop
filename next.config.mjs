d7645849eeb95553253a73c14b9492ef75642042
丁一娉 <dingyp@intellimesh.io>
2024-04-02 14:14:34 +0800

    hos#805 删除不必要的config

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
     transpilePackages: ['@ant-design', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-tree', 'rc-table'], // 第三方的依赖
     reactStrictMode: true,
     webpack: (config) => {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            fs: false,
-        };
-
         return config;
     },
 };
